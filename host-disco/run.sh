d8e1bae973a011a9aa84589067c7578588eb224b
kokke <spam@rowdy.dk>
2021-07-14 22:24:02 +0200

    Update run.sh

--- host-disco/run.sh
+++ host-disco/run.sh
@@ -1,3 +1,3 @@
 # unix version - uses `pwd` to get absolute path for CWD
 docker network create docker-host-discovery_mqtt-network
-docker run --network=docker-host-discovery_mqtt-network -p 1500:1500 -v `pwd`:/tmp -w /tmp alpine sh /tmp/t.sh
+docker run --network=docker-host-discovery_mqtt-network -p 1500:1500 -v `pwd`:/tmp -w /tmp alpine sh /tmp/icmp_discov_httpd.sh.sh
