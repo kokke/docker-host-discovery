a9e802959299031d94213faff04d0e5c03bfdfb2
kokke <spam@rowdy.dk>
2021-07-14 22:24:15 +0200

    Update run.cmd

--- host-disco/run.cmd
+++ host-disco/run.cmd
@@ -1,3 +1,3 @@
 rem windowsversion - uses %cd% to get absolute path for CWD
 docker network create docker-host-discovery_mqtt-network
-docker run --network=docker-host-discovery_mqtt-network -p 1500:1500 -v %cd%:/tmp -w /tmp alpine sh /tmp/t.sh
+docker run --network=docker-host-discovery_mqtt-network -p 1500:1500 -v %cd%:/tmp -w /tmp alpine sh /tmp/icmp_discov_httpd.sh.sh
